4658e97513fd0a54aaeba5d3a63a8f9a0d33224a
SZBALINTT <balint.szabon@stud.uni-corvinus.hu>
2025-05-01 18:06:39 +0200

    Kontroller létrehozva, Hello World API Teszt

---
+++ Modul/dnn-tegnaprakv-recept-ajanlo/Dnn.TegnapraKV.ReceptAjanlo/Classes/ModuleController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using DotNetNuke.Entities.Tabs;
+using DotNetNuke.Web.Api;
+using System.Linq;
+using System.Collections.Generic;
+using DotNetNuke.Services.Connections;
+using DotNetNuke.Common.Utilities;
+using System.Web;
+
+
+namespace dnn.tegnaprakv.Dnn.TegnapraKV.ReceptAjanlo.Classes
+{
+    public class ModuleController:DnnApiController
+    {
+
+        [AllowAnonymous]
+        [HttpGet]
+        public HttpResponseMessage HelloWorld()
+        {
+            return Request.CreateResponse(HttpStatusCode.OK, "Hello World!");
+        }
+
+
+
+    }
+}
---
+++ Modul/dnn-tegnaprakv-recept-ajanlo/Dnn.TegnapraKV.ReceptAjanlo/Classes/RouteMapper.cs
@@ -0,0 +1,17 @@
+﻿using DotNetNuke.Web.Api;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DotNetNuke.Web.Api;
+
+namespace dnn.tegnaprakv.Dnn.TegnapraKV.ReceptAjanlo.Classes
+{
+    public class RouteMapper:IServiceRouteMapper
+    {
+        public void RegisterRoutes(IMapRoute mapRouteManager)
+        {
+            mapRouteManager.MapHttpRoute("Dnn.TegnapraKV.ReceptAjanlo", "default", "{controller}/{action}", new[] { "dnn.tegnaprakv.Dnn.TegnapraKV.ReceptAjanlo.Classes" });
+        }
+    }
+}
