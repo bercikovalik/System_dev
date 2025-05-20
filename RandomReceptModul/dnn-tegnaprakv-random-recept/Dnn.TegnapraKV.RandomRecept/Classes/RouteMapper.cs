c0b6f912323e7a4f21dd8940626609f0d96c2461
SZBALINTT <szabon.balint@gmail.com>
2025-05-20 13:23:47 +0200

    A random recept modul kísérletet elvetettük, helyette a fő modulban található a funkcionalitása, fájlok törlésre kerülnek

--- RandomReceptModul/dnn-tegnaprakv-random-recept/Dnn.TegnapraKV.RandomRecept/Classes/RouteMapper.cs
+++
@@ -1,9 +0,0 @@
-using DotNetNuke.Web.Api;
-
-public class RouteMapper : IServiceRouteMapper
-{
-    public void RegisterRoutes(IMapRoute mapRouteManager)
-    {
-        mapRouteManager.MapHttpRoute("Dnn.TegnapraKV.RandomRecept", "default", "{controller}/{action}", new[] { "dnn.tegnaprakv.Dnn.TegnapraKV.RandomRecept.Classes" });
-    }
-}
